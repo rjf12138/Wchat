4b041eaafd23a70aa7215650d061c03d65c6ed78
rjf12138 <3161173206@qq.com>
2019-12-08 22:46:14 +0800

    2019年 12月 08日 星期日 22:46:00 CST

--- Wchat_server/network/buffer.h
+++ Wchat_server/network/buffer.h
@@ -1,19 +1,20 @@
 /*
 *   start_pos 指向第一个可读/写位置， end_pos 指向最后一个可读写之后的位置
 *   整个buffer采用循环队列的方式保存数据
+*   可能还要考虑高低字节的顺序，目前先不考虑
 */
 
 #ifndef __BUFFER_H__
 #define __BUFFER_H__
 
 #include "basic_head.h"
+#include "mutex.h"
 
-#define MAX_BUFFER_SIZE 1024
 #define MAX_STRING_SIZE 512
 
 class Buffer {
 public:
-    Buffer();
+    Buffer(int max_buffer_size = 1024);
     virtual ~Buffer();
 
     int read_int8(int8_t &val);
@@ -28,46 +29,44 @@ public:
     int write_int32(int32_t val);
     int write_int64(int64_t val);
     int write_string(string str);
-    int write_bytes(const void *buf, int buf_size);
+    int write_bytes(const void *buf, int buf_size, bool match = false);
 
     int read_int8_lock(int8_t &val);
     int read_int16_lock(int16_t &val);
     int read_int32_lock(int32_t &val);
     int read_int64_lock(int64_t &val);
     int read_string_lock(string &str);
-    int read_bytes_lock(void *buf, int buf_size);
+    int read_bytes_lock(void *buf, int buf_size, bool match = false);
 
     int write_int8_lock(int8_t val);
     int write_int16_lock(int16_t val);
     int write_int32_lock(int32_t val);
     int write_int64_lock(int64_t val);
     int write_string_lock(string str);
-    int write_bytes_lock(const void *buf, int buf_size);
+    int write_bytes_lock(const void *buf, int buf_size, bool match = false);
 
-    bool empty(void);
-    bool full(void);
-    int size(void);
+    bool empty(void) const;
+    bool full(void) const;
+    int data_size(void) const;
+    int idle_size() const;
     int clear(void);
-    int resize(int size);
-private:
-    bool check_read_pos(void);
-    bool check_write_pos(void);
 
-    void next_read_start_pos(int offset = 1);
-    void next_read_end_pos(int offset = 1);
-    void next_write_start_pos(int offset = 1);
-    void next_write_end_pos(int offset = 1);
 private:
-    vector<char> write_buffer_;
-    vector<char> read_buffer_;
+    void next_read_pos(int offset = 1);
+    void next_write_pos(int offset = 1);
+
+    int copy_data_to_buffer(const void *data, int size);
+    int copy_data_from_buffer(void *data, int size);
+
+private:
+    vector<int8_t> buffer_;
+    Mutex lock_;
 
     int start_read_pos_;
-    int end_read_pos_;
     int start_write_pos_;
-    int end_write_pos_;
 
-    int write_buffer_size_;
-    int read_buffer_size_;
+    int data_size_;
+    int max_buffer_size_;
 };
 
 #endif
